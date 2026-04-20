c4a115918299466f708482bcd167e390c7c13c46
achraf boumessouer <achraf.boumessouer@ardeotis.fr>
2026-04-20 16:48:35 +0200

    feature: modify first page landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './faetures/home/pages/home/home';
+import { AuthLoginComponent } from './faetures/auth-login/auth-login.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Home
+        component: AuthLoginComponent
     }
 ];
