b7af2795274ba797dcac3667c1553d23adabd82f
achraf boumessouer <achraf.boumessouer@ardeotis.fr>
2026-06-15 17:37:08 +0200

    feature:error handling

--- src/app/guards/auth.interceptor.ts
+++ src/app/guards/auth.interceptor.ts
@@ -1,7 +1,13 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
+import {MatSnackBar} from '@angular/material/snack-bar'
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('token');
+    const snackbar = inject(MatSnackBar);
+    const router = inject(Router);
     
     if(token) {
         const cloned = req.clone({
@@ -11,5 +17,28 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         });
         return next(cloned);
     }
-    return next(req);
+    return next(req).pipe(
+        catchError((error : HttpErrorResponse) => {
+            let message = 'Une erreur est survenue';
+            if(error.status === 0){
+                message = 'Impossible de joindre le serveur'
+            } else if(error.status === 401){
+                localStorage.removeItem('token')
+                router.navigate(['/login']);
+            } else if(error.status === 403){
+                message = "Vous n'avez pas le droit"
+            } else if( error.status >= 400 && error.status < 500) {
+                message = error.error?.message || 'Erreur lors de la reqsute '
+            } else if(error.status >= 500){
+                message = 'Erreur serveur veuillez réessayer'
+            }
+
+            snackbar.open(message, 'Fermer', {
+                duration: 5000,
+                horizontalPosition: 'right',
+                verticalPosition: 'top'
+            })
+            return throwError(() => error);
+        })
+    );
 }
