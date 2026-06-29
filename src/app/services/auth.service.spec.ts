002995bec1259278db1f84171016b67d1d3c1e79
achraf boumessouer <achraf.boumessouer@ardeotis.fr>
2026-06-29 17:14:10 +0200

    feature: unit tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -28,4 +28,50 @@ describe('AuthService',() => {
     it('should be created', () => {
         expect(service).toBeTruthy()
     })
+
+    it('should login an store token in localstorage',() =>{
+        const mockResponse = { token: 'fake'};
+        service.login('test@fmail.com','test').subscribe(res => {
+            expect(res).toEqual(mockResponse);
+            expect(localStorage.getItem('token')).toBe('fake')
+        })
+    const req = httpMock.expectOne(`${api}/login`)
+    expect(req.request.method).toBe('POST')
+    expect(req.request.body).toEqual({
+        email: 'test@fmail.com',
+        password: 'test'
+    })
+    req.flush(mockResponse)
+    })
+
+     it('should reggister and store token on localstorage',() =>{
+        const mockResponse = { token: 'fake'};
+        service.register('test@gmail.com','test','achraf','bms').subscribe(res => {
+            expect(res).toEqual(mockResponse)
+            expect(localStorage.getItem('token')).toBe('token')
+        })
+    const req = httpMock.expectOne(`${api}/register`)
+    expect(req.request.method).toBe('POST')
+    expect(req.request.body).toEqual({
+        email: 'test@fmail.com',
+        password: 'test',
+        firstName:'achraf',
+        lastName:'bms'
+    })
+    req.flush(mockResponse)
+    })
+
+     it('should return null when token does not exist',() =>{
+      
+    expect(service.getToken).toBeNull()
+    
+    })
+
+      it('should return true when user is authenticated',() =>{
+      localStorage.setItem('token', 'token')
+    expect(service.isAuthenticated).toBeTrue()
+    
+    })
+
+    
 })
---
+++ src/app/services/position.service.spec.ts
@@ -0,0 +1,61 @@
+
+import { TestBed } from '@angular/core/testing';
+import {PositionnementService} from './position.service'
+import { HttpTestingController, HttpClientTestingModule} from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http';
+describe('PositionService',() => {
+    let service:PositionnementService;
+    let httpMock: HttpTestingController
+
+    const api = 'http//localhost:8080/auth';
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers:[PositionnementService]
+        })
+    })
+
+    service = TestBed.inject(PositionnementService);
+    httpMock = TestBed.inject(HttpTestingController)
+   
+
+    afterEach(() => {
+        httpMock.verify()
+       
+    })
+
+    it('should be created', () => {
+        expect(service).toBeTruthy()
+    })
+
+    it('should get all missions with pagination',() =>{
+      const mockResponse = {
+        content:[],
+        totalElements: 0,
+        totalPages: 0,
+        size: 10,
+        number: 0
+      }
+      service.getAllMissions(0,10).subscribe(res => {
+        expect(res).toEqual(mockResponse as any)
+      })
+
+      const req = httpMock.expectOne(`${api}?page=0&size=10`)
+      expect(req.request.method).toBe('GET')
+      req.flush(mockResponse)
+    })
+
+     it('should get mission by id ',() =>{
+        const mockResponse = { id: '1',title: 'Mission Java Angular'};
+        service.getById('1').subscribe(res => {
+            expect(res).toEqual(mockResponse as any)
+        
+        })
+    const req = httpMock.expectOne(`${api}/1`)
+    expect(req.request.method).toBe('GET')
+    req.flush(mockResponse)
+    })
+
+    
+})
